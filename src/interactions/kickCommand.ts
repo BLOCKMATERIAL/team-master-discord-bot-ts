5001821ecbae0b845eb37ca73ef2673448e92310
Myroslav Hryschenko <miroslavgrischenko@gmail.com>
2024-08-08 20:59:11 +0300

    Added send message about kick player from team

--- src/interactions/kickCommand.ts
+++ src/interactions/kickCommand.ts
@@ -1,5 +1,6 @@
 import { ChatInputCommandInteraction, Client } from "discord.js";
 import { getTeamIdByLeader, teams, updateTeamMessage } from "../utils";
+import { logger } from "..";
 
 export async function handleKickCommand(interaction: ChatInputCommandInteraction) {
     const teamId = getTeamIdByLeader(interaction.user.id);
@@ -20,16 +21,24 @@ export async function handleKickCommand(interaction: ChatInputCommandInteraction
         return;
     }
 
+    const wasInReserve = team.reserve.includes(playerToKick.id);
     team.players = team.players.filter(id => id !== playerToKick.id);
     team.reserve = team.reserve.filter(id => id !== playerToKick.id);
 
-    if (team.players.length < 5 && team.reserve.length > 0) {
+    if (!wasInReserve && team.reserve.length > 0 && team.players.length < team.slots) {
         const newPlayer = team.reserve.shift()!;
         team.players.push(newPlayer);
+        await interaction.client.users.cache.get(newPlayer)?.send(`Вас переміщено з резерву до активного складу команди ${teamId}.`);
     }
 
     await updateTeamMessage(interaction.client, teamId);
+
+    try {
+        await playerToKick.send(`Вас було виключено з команди ${teamId}.`);
+        logger.info(`User ${playerToKick.id} was kicked from team ${teamId} and notified.`);
+    } catch (error) {
+        logger.warn(`Failed to send kick notification to user ${playerToKick.id}: ${error}`);
+    }
+
     await interaction.reply({ content: `Гравця ${playerToKick} вигнано з команди.`, ephemeral: true });
 }
-
-
